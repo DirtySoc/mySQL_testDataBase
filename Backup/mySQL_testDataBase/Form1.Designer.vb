f66ef5f3eb84c601acd4417c24dacf104a608f59
Derek <33207400+DirtySoc@users.noreply.github.com>
2018-03-26 16:02:05 -0400

    Initial Commit with project.

---
+++ Backup/mySQL_testDataBase/Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.txt_firstName = New System.Windows.Forms.TextBox
+        Me.txt_lastName = New System.Windows.Forms.TextBox
+        Me.saveButton = New System.Windows.Forms.Button
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox
+        Me.listBox_users = New System.Windows.Forms.ListBox
+        Me.getButton = New System.Windows.Forms.Button
+        Me.deleteButton = New System.Windows.Forms.Button
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.AutoSize = True
+        Me.GroupBox1.Controls.Add(Me.saveButton)
+        Me.GroupBox1.Controls.Add(Me.txt_lastName)
+        Me.GroupBox1.Controls.Add(Me.txt_firstName)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(7, 7)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(381, 76)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "INSERT DATA"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "First Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(137, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Last Name"
+        '
+        'txt_firstName
+        '
+        Me.txt_firstName.Location = New System.Drawing.Point(10, 37)
+        Me.txt_firstName.Name = "txt_firstName"
+        Me.txt_firstName.Size = New System.Drawing.Size(115, 20)
+        Me.txt_firstName.TabIndex = 2
+        '
+        'txt_lastName
+        '
+        Me.txt_lastName.Location = New System.Drawing.Point(140, 37)
+        Me.txt_lastName.Name = "txt_lastName"
+        Me.txt_lastName.Size = New System.Drawing.Size(112, 20)
+        Me.txt_lastName.TabIndex = 3
+        '
+        'saveButton
+        '
+        Me.saveButton.Location = New System.Drawing.Point(270, 34)
+        Me.saveButton.Name = "saveButton"
+        Me.saveButton.Size = New System.Drawing.Size(75, 23)
+        Me.saveButton.TabIndex = 4
+        Me.saveButton.Text = "SAVE"
+        Me.saveButton.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.deleteButton)
+        Me.GroupBox2.Controls.Add(Me.getButton)
+        Me.GroupBox2.Controls.Add(Me.listBox_users)
+        Me.GroupBox2.Location = New System.Drawing.Point(7, 90)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(381, 171)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "SELECT DATA"
+        '
+        'listBox_users
+        '
+        Me.listBox_users.FormattingEnabled = True
+        Me.listBox_users.Location = New System.Drawing.Point(10, 46)
+        Me.listBox_users.Name = "listBox_users"
+        Me.listBox_users.Size = New System.Drawing.Size(365, 121)
+        Me.listBox_users.TabIndex = 0
+        '
+        'getButton
+        '
+        Me.getButton.Location = New System.Drawing.Point(300, 17)
+        Me.getButton.Name = "getButton"
+        Me.getButton.Size = New System.Drawing.Size(75, 23)
+        Me.getButton.TabIndex = 1
+        Me.getButton.Text = "GET"
+        Me.getButton.UseVisualStyleBackColor = True
+        '
+        'deleteButton
+        '
+        Me.deleteButton.Location = New System.Drawing.Point(120, 17)
+        Me.deleteButton.Name = "deleteButton"
+        Me.deleteButton.Size = New System.Drawing.Size(132, 23)
+        Me.deleteButton.TabIndex = 2
+        Me.deleteButton.Text = "DELETE SELECTED"
+        Me.deleteButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(398, 269)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "mySQL DB TEST"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents saveButton As System.Windows.Forms.Button
+    Friend WithEvents txt_lastName As System.Windows.Forms.TextBox
+    Friend WithEvents txt_firstName As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents listBox_users As System.Windows.Forms.ListBox
+    Friend WithEvents getButton As System.Windows.Forms.Button
+    Friend WithEvents deleteButton As System.Windows.Forms.Button
+
+End Class
---
+++ mySQL_testDataBase/Form1.vb
@@ -0,0 +1,93 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+
+    Public dbconn As New MySqlConnection
+    Public sql As String
+    Public dbcomm As New MySqlCommand
+    Public dbread As MySqlDataReader
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        dbconn = New MySqlConnection("Data Source=localhost;user id=test;password=test123;port=3306;database=test")
+        Try
+            dbconn.Open()
+        Catch ex As Exception
+            MsgBox("Connection Error: " + ex.Message.ToString)
+        End Try
+    End Sub
+
+    Private Sub saveButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles saveButton.Click
+        addUser(txt_firstName.Text.ToString(), txt_lastName.Text.ToString())
+    End Sub
+
+    Private Sub getButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles getButton.Click
+        getUserTableData()
+    End Sub
+
+    Private Sub deleteButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles deleteButton.Click
+        Dim userIsSure = MessageBox.Show("Are you sure you want to remove the selected from the database?", "caption", MessageBoxButtons.YesNo)
+        If userIsSure = DialogResult.Yes Then
+            'Grab the selected user and split the sting into firstname and lastname.
+            Dim fullName As String = listBox_users.SelectedItem.ToString()
+            Dim name() As String = fullName.Split(" ")
+            'pass deleteUser the first name and last name of person getting deleted.
+            deleteUser(name(0), name(1))
+            getUserTableData()
+        Else
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub addUser(ByVal firstName, ByVal lastName)
+        sql = "INSERT INTO _users(u_id,u_firstName,u_lastName) VALUES(NULL,'" & firstName & "','" & lastName & "')"
+        Try
+            dbcomm = New MySqlCommand(sql, dbconn)
+            dbread = dbcomm.ExecuteReader()
+            dbread.Close()
+            MsgBox("Data inserted.")
+            txt_firstName.Clear()
+            txt_lastName.Clear()
+        Catch ex As Exception
+            MsgBox("Failed to insert data: " & ex.Message.ToString())
+        End Try
+        dbread.Close()
+    End Sub
+
+    ''' <summary>
+    ''' DELETE FROM test 
+    ''' WHERE u_firstname='Bob'
+    ''' AND u_lastname='Burger'
+    ''' </summary>
+    ''' <remarks>
+    ''' NOT WORKING 
+    ''' Sub needs to be re-written to get u_id and then delete user based on id not name.
+    ''' </remarks>
+    Private Sub deleteUser(ByVal firstName, ByVal lastName)
+        sql = "DELETE FROM _users WHERE u_firstname = '" & firstName & "' AND u_lastname = '" & lastName & "' LIMIT 1"
+        'MsgBox(sql)
+        Try
+            dbcomm = New MySqlCommand(sql, dbconn)
+            dbcomm.ExecuteNonQuery()
+            MsgBox("User deleted!")
+        Catch ex As Exception
+            MsgBox("Error trying to delete: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub getUserTableData()
+        listBox_users.Items.Clear()
+        sql = "SELECT * FROM _users"
+        Try
+            dbcomm = New MySqlCommand(sql, dbconn)
+            dbread = dbcomm.ExecuteReader()
+            While dbread.Read
+                listBox_users.Items.Add(dbread("u_firstName").ToString() & " " & dbread("u_lastName").ToString())
+            End While
+        Catch ex As Exception
+            MsgBox("Problem loading data: " & ex.Message.ToString)
+        End Try
+        dbread.Close()
+    End Sub
+End Class
+
